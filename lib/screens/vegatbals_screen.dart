18cdbbc633701711ddeb130f39ff0030f1c9975f
Mohamedwaelfaro <100936115+Mohamedwaelfaro@users.noreply.github.com>
2024-07-09 22:33:07 +0300

    fix ui

--- lib/screens/vegatbals_screen.dart
+++ lib/screens/vegatbals_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class VegatbalsScreen extends StatefulWidget {
   const VegatbalsScreen({super.key});
@@ -13,44 +15,62 @@ class _VegatbalsScreenState extends State<VegatbalsScreen> {
     return Scaffold(
       body: Column(
         children: [
-          Row(
-            children: [
-              SizedBox(
-                width: 20,
-              ),
-              Padding(
-                // padding: const EdgeInsets.only(left: 20),
-                padding: EdgeInsetsDirectional.only(start: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+          SizedBox(
+            height: 215,
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              children: [
+                Row(
                   children: [
-                    Icon(Icons.arrow_back_ios_new),
-                    Text(
-                      'vegatbals',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w700,
+                    //SizedBox(width: 20,),
+                    Padding(
+                      // padding: const EdgeInsets.only(left: 20),
+                      padding: EdgeInsetsDirectional.only(
+                          start: 20, top: kToolbarHeight),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Icon(Icons.arrow_back_ios_new),
+                          Text(
+                            'vegatbals',
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.w700),
+                          ),
+                        ],
                       ),
                     ),
+                    Spacer(),
+                    Image.asset('assets/Vegetables.png'),
                   ],
                 ),
-              ),
-              Spacer(),
-              Image.asset('assets/Vegetables.png'),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Search',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
+                PositionedDirectional(
+                  bottom: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: SizedBox(
+                      height: 48,
+                      width: MediaQuery.of(context).size.width,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.search_sharp),
+                          hintText: "search",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(27),
+                            borderSide: BorderSide(
+                              width: 1,
+                              color: Color(0xfffD9D0E3),
+                            ),
+                          ),
+                          fillColor: Color(0xffffffff),
+                          filled: true,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-          )
+          ),
         ],
       ),
     );
